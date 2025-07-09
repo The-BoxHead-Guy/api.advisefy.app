6afeb9bbe3e0f7a627db196d740fad6cd12b2a89
The-BoxHead-Guy <astralgmd@gmail.com>
2025-07-08 22:08:33 -0400

    🔧 refactor: Update API route to use namespaced PiecesOfAdvicesController.
    - Changed the import statement for PiecesOfAdvicesController to use the namespaced version under App\Http\Controllers\Api\V1.

--- routes/api/v1/v1.php
+++ routes/api/v1/v1.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\V1\PiecesOfAdvicesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PiecesOfAdvicesController;
 
 Route::apiResource('pieces-of-advices', PiecesOfAdvicesController::class);
