55c9e8aa88a753007f66902edd65b1e6c7c58ed5
The-BoxHead-Guy <astralgmd@gmail.com>
2025-07-08 22:11:46 -0400

    🔧 feat: Create PiecesOfAdvices model for database interactions.
    - Introduced the PiecesOfAdvices model to interact with the pieces_of_advices table.
    - Set the default pagination to 20 items per page.

---
+++ app/Models/PiecesOfAdvices.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PiecesOfAdvices extends Model
+{
+    protected $table = 'pieces_of_advices';
+
+    protected $perPage = 20;
+
+    
+}
