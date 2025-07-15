1a436200b26e5213c653fed0da4717a900ecbc05
The-BoxHead-Guy <astralgmd@gmail.com>
2025-07-14 22:07:54 -0400

    🔧 refactor: Update logging implementation in test route.
    - Replaced repetitive error log statements with a new Logger trait instance.
    - Utilized various logging methods (emergency, alert, critical, error, warning, notice, info, debug) for improved logging clarity and functionality.
    - Enhanced the /logs route for better testing of different log levels.

--- routes/test/test.php
+++ routes/test/test.php
@@ -6,13 +6,18 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/logs', function () {
-    Log::error('Wait... This is huge');
-    Log::error('Wait... This is huge');
-    Log::error('Wait... This is huge');
-    Log::error('Wait... This is huge');
-    Log::error('Wait... This is huge');
-    Log::error('Wait... This is huge');
-    Log::error('Wait... This is huge');
+    $logger = new class {
+        use \App\Traits\Logger;
+    };
+
+    $logger->logEmergency('This is an emergency log');
+    $logger->logAlert('This is an alert log');
+    $logger->logCritical('This is a critical log');
+    $logger->logError('This is an error log');
+    $logger->logWarning('This is a warning log');
+    $logger->logNotice('This is a notice log');
+    $logger->logInfo('This is an info log');
+    $logger->logDebug('This is a debug log');
 
     return response()
         ->json(
