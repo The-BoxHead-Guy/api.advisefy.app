8d46c2935cd7ad659931f111bdd427cedf4cfc8f
The-BoxHead-Guy <astralgmd@gmail.com>
2025-07-17 21:16:28 -0400

    🏗️ feat: Add API configuration file for versioning.
    - Introduced api.php configuration file to define the API version.
    - Set default API version to 'v1.0.0', configurable via environment variable.

---
+++ config/api.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Api Version
+    |--------------------------------------------------------------------------
+    |
+    | This value represents the version of the API, which is going to be
+    | updated to match repository's one as releases.
+    |
+    */
+
+    'version' => env('API_VERSION', 'v1.0.0'),
+];
