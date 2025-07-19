f827750a7018f10d37b7dc9e6598b967127213db
The-BoxHead-Guy <astralgmd@gmail.com>
2025-07-18 21:27:11 -0400

    feat: Add find method to PiecesOfAdvicesRepositoryInterface for retrieving advice by ID.

--- app/Repositories/Contracts/PiecesOfAdvicesRepositoryInterface.php
+++ app/Repositories/Contracts/PiecesOfAdvicesRepositoryInterface.php
@@ -7,4 +7,5 @@ use App\Models\PiecesOfAdvices;
 interface PiecesOfAdvicesRepositoryInterface
 {
     public function create(array $data): PiecesOfAdvices;
+    public function find(int $id): ?PiecesOfAdvices;
 }
